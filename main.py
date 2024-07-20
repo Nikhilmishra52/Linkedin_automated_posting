e319f52177e162ed95d25cb3d67927cf7d9e7439
Nikhil <nm47855r@gmail.com>
2024-07-20 23:46:27 +0530

    added scheduler

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os.path
-
+import schedule
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import pickle
+import functools
 driver_path=r"C:\Users\yogesh\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"
 content_path=r"C:\Users\yogesh\OneDrive\Desktop\Linkedin_automated_posting\Content"
 
@@ -75,6 +76,19 @@ def post_to_feed(driver,file_to_post):
             )
             Add_media.click()
             time.sleep(5)
+            file_input = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "/html/body/div[3]/div/div/div/div[2]/div/div/div[1]/div/div[2]/input"))
+            )
+            file_input.send_keys(file_to_post)
+            time.sleep(3)
+
+            next_btn = WebDriverWait(driver, 60).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "/html/body/div[3]/div/div/div/div[2]/div/div/div[2]/div/button[2]"))
+            )
+            next_btn.click()
+            time.sleep(3)
         elif file_Extension in [".txt"]:
             with open(file_to_post,'r') as txt_file:
                 Text=txt_file.read()
@@ -109,16 +123,21 @@ def post_to_feed(driver,file_to_post):
             add_document_btn.click()
             time.sleep(3)
 
-        file_input=WebDriverWait(driver,60).until(
-            EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div/div/div[1]/div/div[2]/input"))
-        )
-        file_input.send_keys(file_to_post)
+            file_input=WebDriverWait(driver,60).until(
+                EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div/div[1]/div[1]/div/div/input"))
+            )
+            file_input.send_keys(file_to_post)
+            document_title=os.path.basename(file_to_post)
+            add_document_title=WebDriverWait(driver,60).until(
+                EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div/div[1]/div[1]/input"))
+            )
+            add_document_title.send_keys(document_title)
 
-        next_btn=WebDriverWait(driver,60).until(
-            EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div/div/div[2]/div/button[2]"))
-        )
-        next_btn.click()
-        time.sleep(3)
+            done_button=WebDriverWait(driver,60).until(
+                EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div/div[2]/div/button[2]"))
+            )
+            done_button.click()
+            time.sleep(3)
 
         post_btn=WebDriverWait(driver,60).until(
             EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div/div/div[2]/div[4]/div/div[2]/button"))
@@ -133,5 +152,12 @@ def post_to_feed(driver,file_to_post):
     except Exception as e:
         print("error postng files: ",e)
 
+schedule.every(2).minutes.do(functools.partial( post_to_feed,driver,file_to_post))
+
+
 if __name__=="__main__":
     post_to_feed(driver,file_to_post)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(3)
